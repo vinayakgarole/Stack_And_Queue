8ab28e6456f6a993e980a5880ca18b7f20f7bdd0
vinayakgarole <vinayakgarole10@gmail.com>
2022-11-25 21:41:57 +0530

    Added: Peak and pop stack

--- src/com/bridgelabz/Stack.java
+++ src/com/bridgelabz/Stack.java
@@ -1,16 +1,54 @@
 package com.bridgelabz;
 
-public class Stack {
+import java.util.Scanner;
 
-    static class Node {
+class Stack {
 
-        int data;
-        Node next;
+    int top;
+    int maxsize = 5;
+    int[] arr = new int[maxsize];
 
-        Node(int data) {
 
-            this.data = data;
-            this.next = null;
+    boolean isEmpty() {
+        return (top < 0);
+    }
+
+    Stack() {
+        top = -1;
+    }
+
+    boolean push(Scanner sc) {
+
+        if (top == maxsize - 1) {
+            System.out.println("Overflow !!");
+            return false;
+        } else {
+            System.out.println("Enter Value");
+            int val = sc.nextInt();
+            top++;
+            arr[top] = val;
+            System.out.println("Item pushed");
+            return true;
+        }
+    }
+
+    boolean pop() {
+
+        if (top == -1) {
+            System.out.println("Underflow !!");
+            return false;
+        } else {
+            top--;
+            System.out.println("Item popped");
+            return true;
+        }
+    }
+
+    void display() {
+        System.out.println("Printing stack elements: ");
+
+        for (int i = top; i >= 0; i--) {
+            System.out.println(arr[i]);
         }
     }
 }
